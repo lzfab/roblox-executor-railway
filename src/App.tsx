f13dd35383704e56cb38108e30c350a1cec0ef65
lzfab <garenanaomebabi@gmail.com>
2025-12-16 04:34:15 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// Stubs for clean download
+const App = () => <div className="p-10 bg-black text-white"><h1>Source Code Ready</h1></div>;
+export default App;
